4bbda0e6ac085bce358d0859e741a5317b42e310
Amin Toorchi <amin.tourchie@gmail.com>
2026-05-02 02:06:50 +0330

    feat: Shadowing - Constansts

--- variables/main.rs
+++ variables/main.rs
@@ -9,4 +9,15 @@ fn main() {
     let mut y = 10;
     y = 20;
     println!("y is now: {}", y);
+
+
+
+    // Shadowing
+    let z = 30;
+    let z = z + 5;
+    println!("z is: {}", z);
+
+    // Constants
+    const MAX_POINTS: u32 = 100_000;
+    println!("Max points: {}", MAX_POINTS);
 }
