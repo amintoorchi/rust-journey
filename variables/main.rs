91f7a5becb03d7cfdde14d7e978ea36309eae15f
Amin Toorchi <amin.tourchie@gmail.com>
2026-05-02 02:03:19 +0330

    feat: variables in rust, mut & ...

---
+++ variables/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Variables
+
+    // Immutable variable
+    let x = 5;
+    println!("x is: {}", x);
+    
+    // Mutable variable
+    let mut y = 10;
+    y = 20;
+    println!("y is now: {}", y);
+}
