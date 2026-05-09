8bed8af814c77184ee11cbfc1249b421302192e4
Amin Toorchi <amin.tourchie@gmail.com>
2026-05-09 11:25:13 +0330

    loops naming

--- basics/loops/loop_names/main.rs
+++ basics/loops/loop_names/main.rs
@@ -1,9 +1,31 @@
+// fn main(){
+//     'loop_one: loop {
+//         println!("Loop => 1");
+//         loop {
+//             println!("Loop => 2");
+//             break 'loop_one;
+//         }
+//     }
+// }
+
+
+
+
 fn main(){
     'loop_one: loop {
         println!("Loop => 1");
         loop {
             println!("Loop => 2");
-            break 'loop_one;
+            loop {
+                println!("Loop => 3");
+                loop {
+                    println!("Loop => 4");
+                    loop {
+                        println!("Loop => 5");
+                        break 'loop_one;
+                    }
+                }
+            }
         }
     }
 }
